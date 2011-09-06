950a157df9635f282df57f86e312bd8a6c0a63b9
Gonçalo Silva <gnorsilva@gmail.com>
2011-09-06 21:53:59 +0100

    replacing assignement files with jars

--- mastersOfJava/2006/javerClassenCase/Plaatje.java
+++
@@ -1,9 +0,0 @@
-/**
- * Enum voor de verschillende waarden/plaatjes van speelkaarten.
- */
-
-public enum Plaatje { 
-	
-	AAS,TWEE,DRIE,VIER,VIJF,ZES,ZEVEN,ACHT,NEGEN,TIEN,BOER,VROUW,HEER; 
-
-}
--- mastersOfJava/2008/trafficCase/ValetParkingAssistant.java
+++
@@ -1,9 +0,0 @@
-
-public interface ValetParkingAssistant {
-    
-    /**
-     * moves the player car out of the parking area.
-     * @param area the parking area.
-     */
-    public void solve(ParkingArea area);
-}
