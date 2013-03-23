72247a6652603a07b3e69d3ce2f4079c684fded7
Gonçalo Silva <gnorsilva@gmail.com>
2013-03-23 17:05:28 +0000

    setting up the elevator kata

---
+++ elevator/java/201303/src/test/java/com/gnorsilva/SomethingShould.java
@@ -0,0 +1,13 @@
+package com.gnorsilva;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+public class SomethingShould {
+
+    @Test
+    public void knowTheTruth() throws Exception {
+        assertTrue(true);
+    }
+}
